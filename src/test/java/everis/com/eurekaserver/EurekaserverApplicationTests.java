390d53044c1833a17dae22f9ac88d21e18c3aa14
Ivan <ivan@correo.com>
2021-11-30 10:23:44 +0100

    first commit

---
+++ src/test/java/everis/com/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package everis.com.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
